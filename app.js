16163256781ead9f37a5317210cd190fb1c4a40a
Vilck Farias <vilck.ciriaco@gmail.com>
2026-09-23 20:25:55 -0300

    feat: adição de estrutura de server

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const button = document.querySelector('button');
+const list = document.querySelector('ul')
 
 
 async function handle() {
-    const response = await fetch('http://localhost:3000');
-    const resp = await response.json();
+    try {
+        const response = await fetch('http://localhost:3000');
+        const resp = await response.json();
     
-    console.log(resp);
-    
+        list.textContent = '';
+        
+        for (let i = 0; i < resp.length; i++) {
+            const item = document.createElement('li')
+            item.textContent = resp[i].name + ' | ' + resp[i].email;
+            list.appendChild(item)
+        };
+    } catch (error) {
+
+    }
 }
 
 button.addEventListener('click', handle);
--- server.js
+++ server.js
@@ -3,16 +3,31 @@ const http = require('node:http');
 http.createServer((request, response) =>
     {
         response.setHeader('Access-Control-Allow-Origin', '*')
+        if (request.url !== '/users'){
+            response.writeHead(
+                404,
+                {'content-type' : 'application/json'}
+            );
+            response.end(JSON.stringify({message: 'Não existente.'}));
+            return;
+        }
         response.writeHead(
             200,
             {'content-type':'application/json'}
         );
         response.end(JSON.stringify(
-            {
-                name: 'vilck farias',
-                email: 'vilck.ciriaco@gmail.com',
-            }
-        ));
+            [
+                {
+                    name: 'vilck farias',
+                    email: 'vilck.ciriaco@gmail.com',
+                }, {
+                    name: 'antinomo',
+                    email: 'vilck@gmail.com',
+                }, {
+                    name: 'farias',
+                    email: 'fa@gmail.com',
+                }
+            ]));
     }
 
 ).listen(3000);
